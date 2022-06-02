49cf11ad125f2c6221b1782d8aea2f665c030677
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-06-02 15:20:31 -0300

    teste3 in js

---
+++ introJs/teste3.js
@@ -0,0 +1,15 @@
+//console.log("Hello World!");
+
+let evenNuns = [];
+function returnEvenValues(array){
+    for(let i = 0; i < array.lenght;){
+        if(array[i] % 2 === 0){
+            evenNuns.push(array[i]);
+        }
+        }
+    }
+    console.log('Numeros pares são: 'evenNuns);
+
+let array = [1, 2 , 4, 5, 7, 8];
+
+returnEvenValues(array);
