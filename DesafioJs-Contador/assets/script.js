b2203e20a771c42dc39b55136f94cffd153fa950
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-06-06 15:53:27 -0300

    Desafio To-Do List

--- DesafioJs-Contador/assets/script.js
+++ DesafioJs-Contador/assets/script.js
@@ -11,3 +11,11 @@ function diminuir() {
     numeroAtualSpan.innerHTML = numeroAtual
 }
 
+function numeroCor(){
+    if (numeroAtual < 0) {
+        element.style.color='red'
+    }
+    else (numeroAtual > 0)
+    element.style.color='green'
+
+}
