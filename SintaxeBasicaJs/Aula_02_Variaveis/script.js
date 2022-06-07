de020e3265fd81d316fc7a002c7b9c7df142e2a6
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-06-07 14:14:32 -0300

    Estudos

---
+++ SintaxeBasicaJs/Aula_02_Variaveis/script.js
@@ -0,0 +1,100 @@
+//Tipos Primitivos
+
+//Boolean
+var trueOrFalse = false
+console.log(trueOrFalse)
+console.log(typeof(trueOrFalse))
+
+//Number
+var numeroQualquer = 1
+console.log(numeroQualquer)
+console.log(typeof(numeroQualquer))
+
+//String
+var nome = 'Bruna'
+console.log(nome)
+console.log(typeof(nome))
+
+//Função
+var funcao = function() {}
+console.log(funcao)
+console.log(typeof(funcao))
+
+//Como Declarar
+var variavel = 'Bruninha'
+variavel = 'Amanda'
+console.log(variavel)
+
+let variavel2 = 'Iasmim'
+variavel2 = 'Yasmim'
+console.log(variavel2)
+
+//Constante
+const salve = 'Carol'
+console.log(salve)
+
+var escopoGlobal = 'Global'
+console.log(escopoGlobal)
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local'
+    console.log(escopoLocalInterno)
+}
+escopoLocal()
+
+
+//Atribuição
+var atribuicao = 'Carol'
+
+//Comparação
+var comparacao = '0' == 0
+console.log(comparacao)
+
+//Comparação Identica
+var comparacaoIdentica = '0' === 0
+console.log(comparacaoIdentica) 
+
+
+
+//Adição
+var adicao = 1 + 1
+console.log(adicao)
+
+//Subtração
+var subtracao = 1 - 1
+console.log(subtracao)
+
+//Multiplicação
+var multiplicacao = 10 * 2
+console.log(multiplicacao)
+
+//Divisão Real
+var divisaoReal = 40 / 15
+console.log(divisaoReal)
+
+//Divisão Inteira = Resto da Divisão
+var divisaoInteira = 299 % 3
+console.log(divisaoInteira)
+
+//Potenciação
+var potenciacao =  2 ** 10
+console.log(potenciacao)
+
+
+//Maior que 
+var maiorQue = 5 > 2
+console.log(maiorQue)
+
+
+//Menor que 
+var menorQue = 2 < 5
+console.log(menorQue)
+
+//Maior ou Igual que 
+var maiorIgual = 6 >= 6
+console.log(maiorIgual)
+
+
+//Menor ou Igual que 
+var menorIgual = 6 <= 3
+console.log(menorIgual)
