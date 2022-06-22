f3fe79d385555eb104c8ca2ede42e921750f760a
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-06-22 14:59:58 -0300

    TEste

---
+++ SintaxeBasicaJs/Aula_03_Array_&_Objetos/script.js
@@ -0,0 +1,10 @@
+//Oque são vetores ou arrays
+
+//Como declarar uma array 
+let array01 = ['string', 1, true]
+console.log(array01)
+
+//Array pode guardar varios tipos de dados
+let array02 = ['string', 1, true, ['array1'], ['array2'], ['array3']]
+console.log(array02)
+
